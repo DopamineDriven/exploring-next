c348bbb2ba1e3fc6d990345bbb4383f7cf10e558
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-14 05:35:58 -0500

    added publicRuntimeConfig defaults

--- client/next.config.js
+++ client/next.config.js
@@ -10,7 +10,13 @@ module.exports = withCSS(
             // only available on server side
         },
         publicRuntimeConfig: {
-            // available on both server and client
+            // Will be available on both server and client
+            RESTURL_SPEAKERS_PROD:
+                "https://www.siliconvalley-codecamp.com/rest/speakers/ps",
+            RESTURL_SPEAKER_PROD:
+                "https://www.siliconvalley-codecamp.com/rest/speaker",
+            RESTURL_SESSIONS_PROD:
+                "https://www.siliconvalley-codecamp.com/rest/sessions"
         },
         webpack(config, options) {
             config.plugins = config.plugins || [];
