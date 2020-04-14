7761d632de0709acec4af4aa9a16c720d63ed9cf
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-14 04:14:26 -0500

    next/link incorporated into speakercard component

--- client/src/SpeakerCard.jsx
+++ client/src/SpeakerCard.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Link from 'next/link';
 
 class SpeakerCard extends Component {
 
@@ -7,6 +8,17 @@ class SpeakerCard extends Component {
             <div>
                 <img className="card-img-top" src={`/static/speakers/Speaker-${this.props.speaker.id}.jpg`}/>
                 <div className="card-body">
+                    <Link
+                        href={{
+                            pathname: "/speaker", query:
+                                {
+                                    speakerId: this.props.speaker.id
+                                }
+                        }}
+                        as={`speaker/${this.props.speaker.id}`}>
+                        <a className="btn btn-lg btn-block btn-outline-primary">
+                            Details</a>
+                    </Link>
                     <h4 className="card-title">{this.props.speaker.userFirstName} {this.props.speaker.userLastName} </h4>
                     <p className="card-text">{this.props.speaker.bioShort}</p>
                 </div>
