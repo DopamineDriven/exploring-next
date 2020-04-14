f5b7e2284d7804238c33a83c56c41ebdb4c10ada
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-14 03:59:03 -0500

    import updates

--- client/pages/speakers.jsx
+++ client/pages/speakers.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import Link from 'next/link';
-// https://nextjs.org/docs/api-reference/next/link
+import SpeakerCard from '../src/SpeakerCard.jsx';
 
 class Speakers extends Component {
 
